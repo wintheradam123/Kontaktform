8a684960f924114c2fe97f3d768a86547573d6b9
wintheradam123 <wintheradam123@gmail.com>
2021-11-09 12:47:18 +0100

    bootstrap carousel virker ikke

--- src/app/produktinformation/Produkt.ts
+++ src/app/produktinformation/Produkt.ts
@@ -3,5 +3,7 @@ export class Produkt {
   static pris = 200;
   static desc = 'Flot billig mus med RGB.';
   static image = 'assets/img/g203-hero.jpg';
+  static image2= 'assets/img/aa.png';
+  static image3='assets/img/rat.jpg';
   static modeller:Array<string> = ['Hvid','Sort','Gennemsigtig'];
 }
--- src/app/produktinformation/produktinformation.component.ts
+++ src/app/produktinformation/produktinformation.component.ts
@@ -8,16 +8,19 @@ import { Produkt } from './Produkt';
 })
 export class ProduktinformationComponent implements OnInit {
   produkt;
-  produktnavn = '';
+  produktnavn;
   pris;
-  desc = '';
+  desc;
   image;
+  image2;
+  image3;
   constructor () {
     this.produktnavn = Produkt.produktnavn;
     this.pris = Produkt.pris;
     this.desc = Produkt.desc;
     this.image = Produkt.image;
-
+    this.image2 = Produkt.image2;
+    this.image3 = Produkt.image3;
   }
   ngOnInit(): void {
     let modelen =
