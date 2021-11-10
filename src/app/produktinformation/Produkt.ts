8f12ea23c9eeea5b3c36434e63621492485117a7
wintheradam123 <wintheradam123@gmail.com>
2021-11-10 14:43:51 +0100

    lignendeprodukter næsten done, mangler lidt css

--- src/app/produktinformation/Produkt.ts
+++ src/app/produktinformation/Produkt.ts
@@ -1,9 +1,34 @@
 export class Produkt {
   static produktnavn = 'Gamer mus';
-  static pris = 200;
+  static pris = 199;
   static desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies ipsum sem, quis ullamcorper justo tristique vitae. Fusce risus nibh, iaculis nec lobortis tempus, facilisis nec nisi. Vivamus purus lacus, pretium tincidunt dictum sit amet, porttitor ac massa. Phasellus at ante non neque consectetur ultricies. Curabitur sed lectus erat. Vestibulum id ipsum ac sem ultricies pellentesque. In at sem gravida, consequat nisi sit amet, viverra risus.';
   static image = 'assets/img/g203-hero.jpg';
   static image2= 'assets/img/aa.png';
   static image3='assets/img/rat.jpg';
   static modeller:Array<string> = ['Hvid','Sort','Gennemsigtig'];
+  static stjerner = 4;
+}
+
+export class LigProdukt1 {
+  static produktnavn = 'Kontormus';
+  static pris = 149;
+  static desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed sapien metus. Integer sed vehicula ligula. Praesent vitae tellus ut ante posuere euismod ac non est. Aenean auctor vel nibh ac laoreet. In fermentum felis at erat sodales feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu lacus fermentum, sollicitudin orci eu, tempor mauris. Sed in sem dictum, aliquet diam porta, ullamcorper sem. Cras venenatis quis mi non cursus. Curabitur fringilla erat porttitor venenatis convallis. Phasellus eget eleifend purus. Nulla pretium est at tincidunt porttitor. Vestibulum non libero nisl.';
+  static image = 'assets/img/aa.png';
+  static stjerner = 2;
+}
+
+export class LigProdukt2 {
+  static produktnavn = 'God mus';
+  static pris = 175;
+  static desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed sapien metus. Integer sed vehicula ligula. Praesent vitae tellus ut ante posuere euismod ac non est. Aenean auctor vel nibh ac laoreet. In fermentum felis at erat sodales feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu lacus fermentum, sollicitudin orci eu, tempor mauris. Sed in sem dictum, aliquet diam porta, ullamcorper sem. Cras venenatis quis mi non cursus. Curabitur fringilla erat porttitor venenatis convallis. Phasellus eget eleifend purus. Nulla pretium est at tincidunt porttitor. Vestibulum non libero nisl.';
+  static image = 'assets/img/mus.png';
+  static stjerner = 3;
+}
+
+export class LigProdukt3 {
+  static produktnavn = 'Hurtig mus';
+  static pris = 20;
+  static desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed sapien metus. Integer sed vehicula ligula. Praesent vitae tellus ut ante posuere euismod ac non est. Aenean auctor vel nibh ac laoreet. In fermentum felis at erat sodales feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu lacus fermentum, sollicitudin orci eu, tempor mauris. Sed in sem dictum, aliquet diam porta, ullamcorper sem. Cras venenatis quis mi non cursus. Curabitur fringilla erat porttitor venenatis convallis. Phasellus eget eleifend purus. Nulla pretium est at tincidunt porttitor. Vestibulum non libero nisl.';
+  static image = 'assets/img/rat.jpg';
+  static stjerner = 5;
 }
--- src/app/produktinformation/produktinformation.component.ts
+++ src/app/produktinformation/produktinformation.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { Produkt } from './Produkt';
+import { LigProdukt1, Produkt, LigProdukt2, LigProdukt3 } from './Produkt';
 
 @Component({
   selector: 'app-produktinformation',
@@ -15,14 +15,58 @@ export class ProduktinformationComponent implements OnInit {
   image;
   image2;
   image3;
+  stjerner;
+
+  ligproduktnavn1;
+  ligpris1;
+  ligdesc1;
+  ligimage1;
+  ligstjerner1;
+
+  ligproduktnavn2;
+  ligpris2;
+  ligdesc2;
+  ligimage2;
+  ligstjerner2;
+
+  ligproduktnavn3;
+  ligpris3;
+  ligdesc3;
+  ligimage3;
+  ligstjerner3;
+
   constructor () {
+    this.getProdukter();
+  }
+
+  private getProdukter() {
     this.produktnavn = Produkt.produktnavn;
     this.pris = Produkt.pris;
     this.desc = Produkt.desc;
     this.image = Produkt.image;
     this.image2 = Produkt.image2;
     this.image3 = Produkt.image3;
+    this.stjerner = Produkt.stjerner;
+
+    this.ligproduktnavn1 = LigProdukt1.produktnavn;
+    this.ligpris1 = LigProdukt1.pris;
+    this.ligdesc1 = LigProdukt1.desc;
+    this.ligimage1 = LigProdukt1.image;
+    this.ligstjerner1 = LigProdukt1.stjerner;
+
+    this.ligproduktnavn2 = LigProdukt2.produktnavn;
+    this.ligpris2 = LigProdukt2.pris;
+    this.ligdesc2 = LigProdukt2.desc;
+    this.ligimage2 = LigProdukt2.image;
+    this.ligstjerner2 = LigProdukt2.stjerner;
+
+    this.ligproduktnavn3 = LigProdukt3.produktnavn;
+    this.ligpris3 = LigProdukt3.pris;
+    this.ligdesc3 = LigProdukt3.desc;
+    this.ligimage3 = LigProdukt3.image;
+    this.ligstjerner3 = LigProdukt3.stjerner;
   }
+
   ngOnInit(): void {
     let modelen =
       Produkt.modeller.map(function(element) {
